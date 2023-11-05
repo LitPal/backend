821db698426fec72724f09110bddcb3a8c2bcb0c
VishnuVelayuthan <vishnuvelayuthan@gmail.com>
2023-11-05 02:11:45 -0800

    Got JWT token working

--- flask-app/app.py
+++ flask-app/app.py
@@ -1,11 +1,14 @@
-from flask import Flask, redirect, render_template, request, session, url_for
+from flask import Flask, request, jsonify
 import jwt
+from flask_cors import CORS
 
 import sys
+import os
 sys.path.append('..')
 
 import util.parser as parser
 import redis
+import datetime
 
 from dotenv import load_dotenv
 
@@ -13,8 +16,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 app = Flask(__name__)
+CORS(app)
 
-redis_db = redis.Redis(host='localhost', port=6379, db=0) 
+redis_db = redis.Redis(host='localhost', port=6379, db=0, charset="utf-8", decode_responses=True) 
+redis_db.hset('user:alice', 'password', 'pw123')
+redis_db.hset('user:bob', 'password', 'pw456')
 
 @app.route("/")
 def home():
@@ -22,50 +28,32 @@ def home():
 
 @app.route("/get-search-queries/<token>/<query>", methods = ["GET"])
 def get_search(token, query):
-	return parser.obtainContents(query)
+	return jsonify(parser.obtainContents(query))
 
-@app.route("get-bot-message/<token>/<query>", methods = ["GET"])
+@app.route("/get-bot-message/<token>/<query>", methods = ["GET"])
 def open_chatbot(token, query):
 	return "penis"
 
 @app.route("/login", methods=["POST"])
 def login():
-	if request.method == 'POST':
-		username = request.form['username']
-		password = request.form['password']
-		
-    user = f"user:{username}"
-    
+    data = request.json
+    username = data['username']
+    password = data['password']
+
+
+    user_key = f"user:{username}"
     exp_pass = redis_db.hget(user_key, "password")
-    
+
     if exp_pass != password:
         return jsonify({"status" : "wrong-password"})
 
     token = jwt.encode({
         "username" : username,
         "exp" : datetime.datetime.utcnow() + datetime.timedelta(minutes=100)
-    }, app.config["SECRET_KEY"])
+    }, os.getenv("SECRET_KEY"))
 
-		# Check credentials
-    if verify_credentials(username, password):
-        session['user'] = username
-        token = jwt.encode({
-            'username': user['username'], 
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(minutes=100)
-        }, app.config['SECRET_KEY'])
-        return 
-    else:
-        return "Invalid username/password"
+    return jsonify({"status" : "authorized", "token" : token + ""})
 
-def verify_credentials(username, password):
-	"""Check credentials against Redis user table"""
-	user_key = f'user:{username}'
-	expected_pass = redis_db.hget(user_key, 'password')
-
-	if expected_pass == password:
-		return True
-	else: 
-		return False
 
 if __name__ == "__main__":
 	app.run(debug=True, port=4000)
